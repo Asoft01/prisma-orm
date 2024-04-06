492340d632a87b3d3738e38d7dba9199538fc6a9
Adeleke Hammed <lekhad19@gmail.com>
2024-04-06 18:35:42 +0100

    finding multiple users

--- script.ts
+++ script.ts
@@ -1,11 +1,65 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({ log: ["query"]})
 
 async function main() {
 //    const user = await prisma.user.create({ data: { name: "Sally" }}); 
 //    console.log(user); 
-    const user = await prisma.user.findMany(); 
+////////////////////////////////////////////////////////////////////////
+    const user = await prisma.user.findMany({
+        include:  {
+            userPreference: true
+        }
+    }); 
     console.log(user);
+//////////////////////////////////////////////////////////////////////
+    // const users = await prisma.user.deleteMany(); 
+//////////////////////////////////////////////////////////////////////
+// await prisma.user.deleteMany()
+// // await prisma.userPreference.deleteMany()
+// const user = await prisma.user.create({
+//     data: {
+//         name: "Kyle", 
+//         email: "kyle@test.com", 
+//         age: 27, 
+//         userPreference: {
+//             create: {
+//                 emailUpdates: true
+//             }
+//         }
+//     }, 
+//     // include: {
+//     //     userPreference: true
+//     // }
+//     select: {
+//         name: true,
+//         // userPreference: true
+//         userPreference: {select: { id : true} }
+//     }
+//     // We can use either include or select! Bhet we can't use both at the same time 
+// });
+// console.log(user);
+/////////////////////////////////////////////////////// Create Many Users ////////////////////////////////////
+
+// await prisma.user.deleteMany(); 
+// const users = await prisma.user.createMany({
+//     data: [
+//         {
+//             name: "Kyle", 
+//             email: "kyle@test.com", 
+//             age: 27
+//         }, 
+//         {
+//             name: "Sally", 
+//             email: "sally@test.com", 
+//             age: 32
+//         }   
+//     ]
+// })
+
+// console.log(users); 
+//////////////////////////////////////////////// Find Unique ////////////////////////////////////////////////////
+// const users = await prisma.user.findUnique()
+
 }
 
 main()
